88c4361467c3764b3c788164ec956df76c276a63
Antoine Harel <dev@antoineharel.fr>
2019-09-19 14:41:04 +0200

    [FIX] Added missing reducer for auth

--- redux/reducers/auth.js
+++ redux/reducers/auth.js
@@ -4,6 +4,8 @@ export const defaultStore = {
 
 export default function(state = [], action) {
 	switch (action.type) {
+		case 'SET_USER':
+			return {...state, user: action.user}
 		default:
 			return state;
 	}
